7fe12aea9a751cb082c24186a43d450728470bcb
Shannon E. Albeke <salbeke@uwyo.edu>
2018-02-24 15:09:14 -0700

    Changed description of KUD

--- R/package-rKIN.R
+++ R/package-rKIN.R
@@ -3,11 +3,12 @@
 #' The rKIN This package applies methods used to estimate animal homerange, but
 #' instead of geospatial coordinates, we use isotopic coordinates. The estimation
 #' methods include: 1) 2-dimensional bivariate normal kernel utilization density
-#' estimator, 2) bivariate normal ellipse estimator, and 3) minimum convex polygon
-#' estimator, all applied to stable isotope data. Additionally, functions to
+#' estimator with multiple bandwidth estimation methods, 2) bivariate normal
+#' ellipse estimator, and 3) minimum convex polygon estimator, all applied to
+#' stable isotope data. Additionally, functions to
 #' determine niche area, polygon overlap between groups and levels (confidence
-#' contours) and plotting capabilities. 
-#' 
+#' contours) and plotting capabilities.
+#'
 #' @section rKIN functions:
 #' The rKIN functions:
 #' estKIN, estEllipse, estMCP, plot.kin, getArea, calcOverlap
